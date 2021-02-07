80c1b007c9ab4bc9771a04df3dcf23a141fb764a
MarcoValeri <marcovaleri@hotmail.it>
2021-02-07 13:14:16 +0000

    Fixed some bugs on words counter but I have to fix the problem realtive to the space

--- src/components/wordscounter/wordscounter.js
+++ src/components/wordscounter/wordscounter.js
@@ -6,13 +6,44 @@ import './wordscounter.scss';
 const WordsCounter = () => {
     
     const [ characters, setCharacters ] = useState("");
-    const [ words, setWords ] = useState("");
+    const [ words, setWords ] = useState([]);
 
     // Events
     const handlerInput = e => {
         setCharacters(e.target.value);
         setWords(e.target.value.split(" "));
     }
+
+    /*
+    * Create a function that gets as
+    * @parameter an array and 
+    * @return number: the lengh of this array, withoud counting the "space element"
+    */
+   const wordsConter = array => {
+
+        let words = 0;
+
+        array.forEach((word, index) => {
+
+            if (word !== "") {
+                words++;
+            }
+
+            if (word === "\n") {
+                words--;
+            }
+
+            // if (word.includes("\n")) {
+            //     words++;
+            // }
+            
+        });
+
+        return words;
+
+   };
+
+    console.log(words);
 
     return (
         <div id="wordscounter-container">
@@ -23,7 +54,7 @@ const WordsCounter = () => {
             </section>  
             <section className="wordscounter-result">
                 <p className="wordscounter-result-characters">Characters<br /><span className="wordscounter-result-value">{characters.length}</span></p>
-                <p className="wordscounter-result-words">Words<br /><span className="wordscounter-result-words">{words.length}</span></p>
+                <p className="wordscounter-result-words">Words<br /><span className="wordscounter-result-value">{wordsConter(words)}</span></p>
             </section>
         </div>
     )
